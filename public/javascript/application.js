7a87b315454e820e36aba290d7df8ed11a722683
Monica Mow <mow.monica@gmail.com>
2016-06-28 16:41:00 -0700

    Set up initial JS functions
    - add document ready
      > check if DOM is loading before app() function called
    
    - add app function for the app as a whole
    - add function from ajax homework to test sending get requests using ajax

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
+var app = function() {
+  $( "#is-this-working" ).text( "This is working. YAYYY." ).show();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $(function () {
+    var button = $('#list-contacts');
+    button.one('click', function () {
+      $.ajax({
+        url: '/contacts',
+        method: 'GET',
+        success: function (morePostsHtml) {
+          button.replaceWith(morePostsHtml);
+        }
+      });
+    });
+  });
+
+}
+
+$(document).ready(function() {
+  console.log("The DOM is now loaded.");
+  app();
 });
