e11ed99f2abe8eb7a7ac52ff23e0501b58e93e74
Monica Mow <mow.monica@gmail.com>
2016-06-28 16:04:53 -0700

    Create seeds file with dummy contacts for testing

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Contact.create(firstname: "Monica", lastname: "Mow", email: "monica@test.com")
+Contact.create(firstname: "John", lastname: "Smith", email: "bob@example.com")
+Contact.create(firstname: "Bonnie", lastname: "Baker", email: "bonbon@gmail.com")
