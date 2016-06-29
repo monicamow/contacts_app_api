dff3557cabce4c28e6bc2961e4432d09d304b2b5
Monica Mow <mow.monica@gmail.com>
2016-06-29 09:07:12 -0700

    Add a getJSON function to retrieve contact details
    - currently user inputs an id into the form field and then contact is found in database based on this id
    - the ruby object from database is converted to json and the data is fetched to js file using getJSON
    - eventually user should be able to show by something other than id???

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,7 @@
 var app = function() {
   console.log("entered the app");
 
-  // load all contacts when button clicked
+  // LIST ALL CONTACTS
   $(function () {
     var button = $('#list-contacts');
     button.one('click', function () {
@@ -20,10 +20,10 @@ var app = function() {
       $('#contact-list').html('');
   });
 
-  // submit form for new contact on click
+  // NEW CONTACT
   $( "#create-new-contact" ).on('click', function(e) { 
     e.preventDefault();
-    // Get values from elements on the page:
+    // Get values from input fields on the page:
     var first = $("#first-name-input").val();
     var last = $("#last-name-input").val();
     var email = $("#email-input").val();
@@ -32,15 +32,29 @@ var app = function() {
 
     console.log("contact variable created");
    
-    // Send the data using post
+    // Send the data using POST
     $.post( "/contacts", contact, function( data ) {
       console.log("inside JQ post()");
-      contact = JSON.parse(data)
-        $('body').append(contact.firstname).find('#creation-notice').show();
-      }
-    );
+      contact = JSON.parse(data) // might not need this
+      $('body').append(contact.firstname).find('#show-new-contact').show();
+      });
    
   });
+
+  // SHOW CONTACT
+  $( "#show-contact").on('click', function(e) {
+    e.preventDefault();
+    // Get value from input field
+    var contactID = $("#contact-id-input").val();
+
+    console.log("contact id saved to variable");
+
+    // Send the data using GET JSON
+    $.getJSON( "/contacts/" + contactID, function( data ) {
+      console.log("id sent to /contacts/:id");
+      $('body').append(data.firstname).find("#show-contact-data").show();
+    });
+  }); 
 
 }
 
