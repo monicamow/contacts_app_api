c7095f935f7962b850283df16020f6bdf3cd7935
Monica Mow <mow.monica@gmail.com>
2016-06-28 17:46:12 -0700

    Create post action to /contacts to create new contact

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,17 @@ end
 get '/contacts' do
   @contacts = (Contact.all).to_json
 end
+
+post '/contacts' do
+  @contact = Contact.new(
+    firstname: params[:firstname],
+    lastname: params[:lastname],
+    email: params[:email],
+    phone_number: params[:phone_number]
+    )
+  if @contact.save
+    flash[:notice] = "Success!"
+  else
+    flash[:notice] = "Failed."
+  end
+end
