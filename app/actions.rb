1bb4ebf04f9192fb30f7048e66bd89b801b1eaae
Monica Mow <mow.monica@gmail.com>
2016-06-28 23:25:23 -0700

    Add a new route to show individual contacts

--- app/actions.rb
+++ app/actions.rb
@@ -3,10 +3,12 @@ get '/' do
   erb :index
 end
 
+# list
 get '/contacts' do
   @contacts = (Contact.all).to_json
 end
 
+# new/create
 post '/contacts' do
   @contact = Contact.new(
     firstname: params[:firstname],
@@ -15,7 +17,18 @@ post '/contacts' do
     phone_number: params[:phone_number]
     )
   if @contact.save
-    puts @contact.to_json
+    puts "inside save"
     @contact.to_json
   end
 end
+
+# show
+get '/contacts/:id' do |id|
+  @contact = Contact.find(id)
+  puts @contact.to_json
+  @contact.to_json
+end
+
+# update/edit
+
+# destroy/delete
